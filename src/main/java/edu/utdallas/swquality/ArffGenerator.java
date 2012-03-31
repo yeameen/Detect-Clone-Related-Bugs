f78fea2ab2c45083ef94ca234ce90207798bfd87
Chowdhury Ashabul Yeameen <yeameen@gmail.com>
2012-03-31 16:01:50 -0500

    Moved file reader to util function

--- src/main/java/edu/utdallas/swquality/ArffGenerator.java
+++ src/main/java/edu/utdallas/swquality/ArffGenerator.java
@@ -1,6 +1,7 @@
 package edu.utdallas.swquality;
 
 import au.com.bytecode.opencsv.CSVReader;
+import edu.utdallas.swquality.util.ArffUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.yaml.snakeyaml.Yaml;
 
@@ -11,8 +12,6 @@ import java.util.*;
  * Generates Arff file from CSV
  */
 public class ArffGenerator {
-
-    private static final int INDEX_FEATURE_NAME = 1;
 
     private static final String HEADING_BEGINNING = "@RELATION ";
     private static final String HEADING_DATA = "@DATA";
@@ -28,56 +27,16 @@ public class ArffGenerator {
             return;
         }
 
+        Map<Integer, CloneProperties> examples = new TreeMap<Integer, CloneProperties>();
+        Map<Integer, String> featureMap = new TreeMap<Integer, String>();
+
+        int classificationIndex = ArffUtil.readCSV(args[0], examples, featureMap);
+
         // read configuration file
         Yaml yaml = new Yaml();
         Map<String, Map<String, ArrayList<String>>> classificationBoundaryMap = (Map<String, Map<String, ArrayList<String>>>) yaml.load(new FileInputStream(new File(FILE_CONFIGURATION)));
 
         for (String classificationBoundaryType : classificationBoundaryMap.keySet()) {
-
-            File inputFile = new File(args[0]);
-            CSVReader reader = new CSVReader(new FileReader(inputFile));
-
-            // Discard the heading lines
-            reader.readNext();
-            String[] lineArrayInput = reader.readNext();
-
-            Map<Integer, CloneProperties> examples = new TreeMap<Integer, CloneProperties>();
-            Map<Integer, String> featureMap = new TreeMap<Integer, String>();
-            int index = 0;
-
-            // Iterate over data dimension
-            while ((lineArrayInput = reader.readNext()) != null) {
-                String featureName = lineArrayInput[INDEX_FEATURE_NAME];
-
-                // If any empty line
-                if (featureName == null || featureName.isEmpty()) {
-                    continue;
-                }
-
-                featureMap.put(index, featureName);
-
-                for (Category category : Category.values()) {
-                    String[] items = lineArrayInput[category.getIndex()].split("[,\\s]+");
-                    for (String item : items) {
-                        if (item.trim().length() == 0) {
-                            continue;
-                        }
-                        int exampleNumber = Integer.parseInt(item.trim());
-                        CloneProperties cloneProperties = examples.get(exampleNumber);
-                        if (cloneProperties == null) {
-                            cloneProperties = new CloneProperties();
-                            cloneProperties.setCategory(category);
-                        }
-                        cloneProperties.addDiffProperties(index);
-                        examples.put(exampleNumber, cloneProperties);
-                    }
-                }
-                index++;
-            }
-
-//            for (Integer exampleNumber : examples.keySet()) {
-//                System.out.println(exampleNumber + ": " + examples.get(exampleNumber));
-//            }
 
             // Create ARFF
             BufferedWriter writer = new BufferedWriter(new FileWriter(classificationBoundaryType + ".arff"));
@@ -117,7 +76,7 @@ public class ArffGenerator {
                 for (String name : classificationBoundaryMap.get(classificationBoundaryType).keySet()) {
                     Set<String> classes = new HashSet<String>(classificationBoundaryMap.get(classificationBoundaryType).get(name));
                     if(classes.contains(classification)) {
-                        writer.write(index + " " + name.toUpperCase());
+                        writer.write(classificationIndex + " " + name.toUpperCase());
                     }
                 }
 
--- src/main/java/edu/utdallas/swquality/util/ArffUtil.java
+++ src/main/java/edu/utdallas/swquality/util/ArffUtil.java
@@ -1,8 +1,82 @@
 package edu.utdallas.swquality.util;
 
+import au.com.bytecode.opencsv.CSVReader;
+import edu.utdallas.swquality.Category;
+import edu.utdallas.swquality.CloneProperties;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * Utility functions for generating arff file
  */
 public class ArffUtil {
+
+    private static final int INDEX_FEATURE_NAME = 1;
+
+    /**
+     * Reads feature data from CSV, populate data structures and return total number of features
+     *
+     * @param inputFileName     Input CSV Filename
+     * @param examples
+     * @param featureMap
+     * @return                  Total number of features read
+     * @throws IOException
+     */
+
+    public static int readCSV(String inputFileName, Map<Integer, CloneProperties> examples, Map<Integer, String> featureMap) throws IOException {
+        int index = 0;
+        CSVReader reader = new CSVReader(new FileReader(new File(inputFileName)));
+
+        if(examples == null) {
+            examples = new TreeMap<Integer, CloneProperties>();
+        }
+
+        if(featureMap == null) {
+            featureMap = new TreeMap<Integer, String>();
+        }
+
+        // Discard the heading lines
+        reader.readNext();
+        reader.readNext();
+
+        String[] lineArrayInput;
+
+        // Iterate over data dimension
+        while ((lineArrayInput = reader.readNext()) != null) {
+            String featureName = lineArrayInput[INDEX_FEATURE_NAME];
+
+            // If any empty line
+            if (featureName == null || featureName.isEmpty()) {
+                continue;
+            }
+
+            featureMap.put(index, featureName);
+
+            for (Category category : Category.values()) {
+                String[] items = lineArrayInput[category.getIndex()].split("[,\\s]+");
+                for (String item : items) {
+                    if (item.trim().length() == 0) {
+                        continue;
+                    }
+                    int exampleNumber = Integer.parseInt(item.trim());
+                    CloneProperties cloneProperties = examples.get(exampleNumber);
+                    if (cloneProperties == null) {
+                        cloneProperties = new CloneProperties();
+                        cloneProperties.setCategory(category);
+                    }
+                    cloneProperties.addDiffProperties(index);
+                    examples.put(exampleNumber, cloneProperties);
+                }
+            }
+            index++;
+        }
+
+        return index;
+    }
 
 }
